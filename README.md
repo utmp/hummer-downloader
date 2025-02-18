4fd3d0106dabd19615315244604dd571869c63b8
AlijonMurodov <alijonmurodov10@gmail.com>
2025-02-18 17:37:46 +0300

    updated

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-![Static Badge](https://img.shields.io/badge/vuejs-green?style=flat&logo=vuedotjs&logoColor=%234FC08D&logoSize=amg&labelColor=%235E686D)
-![Static Badge](https://img.shields.io/badge/Vite-%23646CFF?style=flat&logo=vite&logoColor=%23646CFF&labelColor=%235E686D)
-![Static Badge](https://img.shields.io/badge/tailwindcss-%2306B6D4?style=flat&logo=tailwindcss&logoColor=%2306B6D4&logoSize=amg&labelColor=%235E686D)
 
-## Vue + Vite + Tailwindcss 
-### Reusable project for frontend devs (***pre-configured***).
+## Hummer downloader 
+### Video downloader (***pre-alpha***).
 ### Clone 
 ``` 
-git clone https://github.com/utmp/vue-vite-tw.git
+git clone https://github.com/utmp/hummer-downloader.git
 ```
 ### Intall
 ```
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { ref,onMounted, computed } from 'vue'
-import {useRouter} from 'vue-router'
+import { ref, onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { FwbButton, FwbInput } from 'flowbite-vue'
-import {useStore} from 'vuex'
+import { useStore } from 'vuex'
 const query = ref('')
 const router = useRouter()
-onMounted(()=>{})
+onMounted(() => {})
 const store = useStore()
-const val1 = computed(()=>store.getters.getVal1)
+const val1 = computed(() => store.getters.getVal1)
 const fetchData = async () => {
   try {
     await store.dispatch('fetchVideoData', query.value)
@@ -19,25 +19,30 @@ const fetchData = async () => {
 </script>
 
 <template>
-     <div class="flex flex-row gap-11 justify-center m-9">
-    <h2 class="text-center text-4xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">Download any video in the internet</h2>
+  <div class="flex flex-col gap-6 justify-center m-4">
+    <h2 class="text-center text-2xl md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
+      Download any video on the internet
+    </h2>
+    <h2 class="text-blue-600 text-center md:text-left">{{ val1 }}</h2>
+    <div class="flex flex-col items-center md:items-start w-full">
+      <fwb-input
+        v-model="query"
+        label="Enter URL"
+        placeholder="https://...."
+        size="lg"
+        class="w-full"
+      >
+        <template #prefix>
+          <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.05025 1.53553C8.03344 0.552348 9.36692 0 10.7574 0C13.6528 0 16 2.34721 16 5.24264C16 6.63308 15.4477 7.96656 14.4645 8.94975L12.4142 11L11 9.58579L13.0503 7.53553C13.6584 6.92742 14 6.10264 14 5.24264C14 3.45178 12.5482 2 10.7574 2C9.89736 2 9.07258 2.34163 8.46447 2.94975L6.41421 5L5 3.58579L7.05025 1.53553Z" fill="#fff"/>
+            <path d="M7.53553 13.0503L9.58579 11L11 12.4142L8.94975 14.4645C7.96656 15.4477 6.63308 16 5.24264 16C2.34721 16 0 13.6528 0 10.7574C0 9.36693 0.552347 8.03344 1.53553 7.05025L3.58579 5L5 6.41421L2.94975 8.46447C2.34163 9.07258 2 9.89736 2 10.7574C2 12.5482 3.45178 14 5.24264 14C6.10264 14 6.92742 13.6584 7.53553 13.0503Z" fill="#fff"/>
+            <path d="M5.70711 11.7071L11.7071 5.70711L10.2929 4.29289L4.29289 10.2929L5.70711 11.7071Z" fill="#fff"/>
+          </svg>
+        </template>
+        <template #suffix>
+          <fwb-button @click="fetchData">Start</fwb-button>
+        </template>
+      </fwb-input>
+    </div>
   </div>
-  <h2 class="text-blue-600">{{ val1 }}</h2>
-    <fwb-input
-      v-model="query"
-      label="Enter URL"
-      placeholder="https://...."
-      size="lg"
-    >
-     <template #prefix>
-<svg class="w-5 h-5 text-gray-500 dark:text-gray-400" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.05025 1.53553C8.03344 0.552348 9.36692 0 10.7574 0C13.6528 0 16 2.34721 16 5.24264C16 6.63308 15.4477 7.96656 14.4645 8.94975L12.4142 11L11 9.58579L13.0503 7.53553C13.6584 6.92742 14 6.10264 14 5.24264C14 3.45178 12.5482 2 10.7574 2C9.89736 2 9.07258 2.34163 8.46447 2.94975L6.41421 5L5 3.58579L7.05025 1.53553Z" fill="#fff"/>
-<path d="M7.53553 13.0503L9.58579 11L11 12.4142L8.94975 14.4645C7.96656 15.4477 6.63308 16 5.24264 16C2.34721 16 0 13.6528 0 10.7574C0 9.36693 0.552347 8.03344 1.53553 7.05025L3.58579 5L5 6.41421L2.94975 8.46447C2.34163 9.07258 2 9.89736 2 10.7574C2 12.5482 3.45178 14 5.24264 14C6.10264 14 6.92742 13.6584 7.53553 13.0503Z" fill="#fff"/>
-<path d="M5.70711 11.7071L11.7071 5.70711L10.2929 4.29289L4.29289 10.2929L5.70711 11.7071Z" fill="#fff"/>
-</svg>
-     </template>
-      <template #suffix>
-        <fwb-button @click="fetchData">Start</fwb-button>
-      </template>
-    </fwb-input>
-  </template>
+</template>
